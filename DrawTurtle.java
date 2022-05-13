dd5e86a7bd2132de25446f7f8e06b1d14fdd15bd
arexxd <60717897+arexxd@users.noreply.github.com>
2022-05-13 15:19:02 -0500

    Updates

--- DrawTurtle.java
+++ DrawTurtle.java
@@ -6,46 +6,71 @@
 import java.awt.*;
 
 public class DrawTurtle extends Turtle {
-	protected Graphics g;
-	protected boolean penstate;
-	protected int centerX;
-	protected int centerY;
-	protected Color c = Color.black;
+ protected Graphics g;
+ protected boolean penstate;
+ protected int centerX;
+ protected int centerY;
+ protected Color c = Color.black;
 
-	public void home() {
-		cx = 0;
-		cy = 0;
-		ch = 0;
-		penstate = true;
-	}
-	
-	
-	public DrawTurtle(Component c) {
-		this.g = c.getGraphics();
-		centerX = c.getSize().width/2;
-		centerY = c.getSize().height/2;
-	}
-	
-	public void setColor(Color color) {
-		c = color;
-	}
-	public Color getColor() { return c;}
-	
-	
-	public void penDown() { penstate = true;}
-	public void penUp() {penstate = false;}
-		
-	public void forward(double d) {
-		double nx = cx + d * Math.sin(ch * Math.PI/180);
-		double ny = cy + d * Math.cos(ch * Math.PI/180);
-		if ( penstate) {
-			Color temp = g.getColor();
-			g.setColor(c);
-			g.drawLine((int)cx+centerX,centerY-(int)cy,(int)nx+centerX,centerY-(int)ny);
-			g.setColor(temp);
-		}
-		cx = nx;
-		cy = ny;
-	}
-	
-}
+ public void home() {
+  cx = 0;
+  cy = 0;
+  ch = 0;
+  penstate = true;
+ }
+ 
+ 
+ public DrawTurtle(Component c) {
+  this.g = c.getGraphics();
+  centerX = c.getSize().width/2;
+  centerY = c.getSize().height/2;
+ }
+ 
+ public void setColor(Color color) {
+  c = color;
+ }
+ public Color getColor() { return c;}
+ 
+ 
+ public void penDown() { penstate = true;}
+ public void penUp() {penstate = false;}
+  
+ public void forward(double d) {
+  double nx = cx + d * Math.sin(ch * Math.PI/180);
+  double ny = cy + d * Math.cos(ch * Math.PI/180);
+  if ( penstate) {
+   Color temp = g.getColor();
+   g.setColor(c);
+   g.drawLine((int)cx+centerX,centerY-(int)cy,(int)nx+centerX,centerY-(int)ny);
+   g.setColor(temp);
+  }
+  cx = nx;
+  cy = ny;
+ }
+ 
+   public void Squiral(int lines, int max, int length)
+  {
+  this.penDown();
+    if (lines==0) {
+      this.left(90);
+      this.forward(length);
+      this.left(90);
+      lines+=1;
+      length+=10;
+      Squiral(lines,max,length);
+    }
+    else if (lines == max-1)
+    {
+      this.forward(length/2);
+    }
+    else {
+      this.forward(length);
+      this.left(90);
+      lines+=1;
+      length+= 10;
+      Squiral(lines,max,length);
+    }
+    this.penUp();
+    }
+  }
+ 
--- SimpleStart.java
+++ SimpleStart.java
@@ -10,79 +10,29 @@ public class SimpleStart extends Applet {
  
  public void paint(Graphics g) {
  
-   
-   /* this is spiral
- DrawTurtle t = new DrawTurtle(this);
- t.home();
- t.penDown();
- spiral(t,5000,50,5);
-  */
-   
-   
-/*
- DrawTurtle t = new DrawTurtle(this);
- t.home();
- t.penDown();
- squirrel(t,20,90,90);
- */
-   /*
- DrawTurtle rish = new DrawTurtle(this);
- rish.home();
- rish.penDown();
- fractal(rish, 8, 150, 40);
-  */
-   
-     DrawTurtle Sierpy = new DrawTurtle(this);
- Sierpy.home();
- Sierpy.penDown();
- sierTriangle(Sierpy, 8, 500, 120);
- 
- 
- 
+  DrawTurtle t = new DrawTurtle(this);
+  t.home();
+  
+  //squiral
+  
+t.Squiral(0, 24, 10);
+  /*
+  int forward = 10;
+  for (int i=0; i<23; i++)
+  {
+  t.left(90);
+  t.forward(forward);
+  forward += 10;
   }
- 
-
- 
- 
- 
- 
- public void spiral(DrawTurtle timmy, int level, double d, int a){
- if(level > 2)
-   spiral (timmy, level -1, d- 0.01, a);
- 
- timmy.forward(d);
- timmy.left(a);
- }
- 
- 
- public void squirrel(DrawTurtle timmy, int level, double d, int a){
- if(level > 2)
-   squirrel (timmy, level -1, d- 5, a);
- 
- timmy.forward(d);
- timmy.left(a);
- }
- 
- public void fractal(DrawTurtle rish, int branches, double d, int angle){
-        if (branches > 1){
-           
-            rish.forward(d);
-            double x = rish.getX();
-            double y = rish.getY();
-            double a = rish.getH();
-           
-            rish.right(angle);
-           
-            fractal(rish, branches - 1, d * .6, angle);;
-     
-           
-            rish.cx = x;
-            rish.cy = y;
-            rish.ch = a;
-
-            rish.left(angle);
-            fractal(rish, branches - 1, d * .6, angle);
-        }
-       
-    }
- 
+  t.left(90);
+  t.forward(forward/2);
+  
+  */
+  
+  //tree fractal
+    
+  }
+  
+   
+  
+}
